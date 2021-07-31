93ca1837426ba3909ede8e42265d7f4b33889f4f
Ratan Bajpai <ratanbajpai@Ratans-MacBook-Pro.local>
2021-07-31 02:18:52 -0400

    Added github gist link

--- js/main.js
+++ js/main.js
@@ -140,19 +140,19 @@ d3.csv("https://gist.githubusercontent.com/ratanbajpai/c193761399371a5b61534f87c
         + "<br>" + "Average annual hours: " + d.avh_2019)
       // .style("left", (event.x)/2 + "px")
       // .style("top", (event.y)/2+30 + "px")
-      // .style("left", (d3.mouse(this)[0]+30) + "px")
-      // .style("top", (d3.mouse(this)[1]+30) + "px")
-      .style("right", d3.select(this).attr("cx") + "px")
-      .style("top", d3.select(this).attr("cy") + "px");
+      .style("left", (d3.mouse(this)[0]+30) + "px")
+      .style("top", (d3.mouse(this)[1]+30) + "px")
+      // .style("right", d3.select(this).attr("cx") + "px")
+      // .style("top", d3.select(this).attr("cy") + "px");
   }
   const moveTooltip = function(event, d) {
     tooltip
       // .style("left", (event.x)/2 + "px")
       // .style("top", (event.y)/2+30 + "px")
-      // .style("left", (d3.mouse(this)[0]+30) + "px")
-      // .style("top", (d3.mouse(this)[1]-10) + "px")
-      .style("right", d3.select(this).attr("cx") + "px")
-      .style("top", d3.select(this).attr("cy") + "px");
+      .style("left", (d3.mouse(this)[0]+30) + "px")
+      .style("top", (d3.mouse(this)[1]+30) + "px")
+      // .style("right", d3.select(this).attr("cx") + "px")
+      // .style("top", d3.select(this).attr("cy") + "px");
   }
   const hideTooltip = function(event, d) {
     tooltip
