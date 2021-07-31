77d702ee9cba8c72f779f09c668d288e56aabc51
Ratan Bajpai <ratanbajpai@Ratans-MacBook-Pro.local>
2021-07-31 13:14:00 -0400

    Added github gist link

--- js/main.js
+++ js/main.js
@@ -112,7 +112,7 @@ d3.csv("https://gist.githubusercontent.com/ratanbajpai/c193761399371a5b61534f87c
       // .style("left", (event.x)/2 + "px")
       // .style("top", (event.y)/2+30 + "px")
       .style("left", (d3.mouse(this)[0]+30) + "px")
-      .style("top", (d3.mouse(this)[1]+30) + "px")
+      .style("top", (d3.mouse(this)[1]+200) + "px")
       // .style("right", d3.select(this).attr("cx") + "px")
       // .style("top", d3.select(this).attr("cy") + "px");
   }
@@ -121,7 +121,7 @@ d3.csv("https://gist.githubusercontent.com/ratanbajpai/c193761399371a5b61534f87c
       // .style("left", (event.x)/2 + "px")
       // .style("top", (event.y)/2+30 + "px")
       .style("left", (d3.mouse(this)[0]+30) + "px")
-      .style("top", (d3.mouse(this)[1]+30) + "px")
+      .style("top", (d3.mouse(this)[1]+200) + "px")
       // .style("right", d3.select(this).attr("cx") + "px")
       // .style("top", d3.select(this).attr("cy") + "px");
   }
