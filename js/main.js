cc4b8a653ceb3ad0e2ecc1024cf1039f96f99af8
Ratan Bajpai <ratanbajpai@Ratans-MacBook-Pro.local>
2021-08-01 08:04:33 -0400

    Added github gist link

--- js/main.js
+++ js/main.js
@@ -85,7 +85,7 @@ const showTooltip = function(d) {
   // .style("left", (event.x)/2 + "px")
   // .style("top", (event.y)/2+30 + "px")
   .style("left", (d3.mouse(this)[0]+275) + "px")
-  .style("top", (d3.mouse(this)[1]+575) + "px")
+  .style("top", (d3.mouse(this)[1]+550) + "px")
   // .style("right", d3.select(this).attr("cx") + "px")
   // .style("top", d3.select(this).attr("cy") + "px");
 }
@@ -94,7 +94,7 @@ const moveTooltip = function(d) {
   // .style("left", (event.x)/2 + "px")
   // .style("top", (event.y)/2+30 + "px")
   .style("left", (d3.mouse(this)[0]+275) + "px")
-  .style("top", (d3.mouse(this)[1]+575) + "px")
+  .style("top", (d3.mouse(this)[1]+550) + "px")
   // .style("right", d3.select(this).attr("cx") + "px")
   // .style("top", d3.select(this).attr("cy") + "px");
 }
