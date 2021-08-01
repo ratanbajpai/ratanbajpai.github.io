9468c82cc82970340d61b1cdb1ad58e58db42a2e
Ratan Bajpai <ratanbajpai@Ratans-MacBook-Pro.local>
2021-08-01 01:50:36 -0400

    Added github gist link

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@ function make_y_gridlines() {
 // Create a tooltip div. This is hidden by default.
 // Add the style to this div through the tooltip class
 // const tooltip = d3.select("#my_dataviz")
-const tooltip = d3.select('#chart_area')
+const tooltip = d3.select('#bubble_plot')
   .append("div")
   .style("opacity", 0)
   .attr("class", "tooltip")
